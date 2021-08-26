7e90fca65efe2bf3535903e28606a1226f73d572
SameNoName <61697143+SameNoName@users.noreply.github.com>
2021-08-26 10:58:17 +0500

    Update and rename 5.py to server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 #каталог с файлами HTML и подкаталогом cgi-bin для сценариев
-webdir = r'C:\Users\marov\Documents\Zfiles\IT\5'
+webdir = r'..\5'
 port = 80
 os.chdir(webdir) #перейти в корневой веб-каталог
 srvraddr = ('', port) # имя хоста (или IP), номер порта
