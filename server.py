8aed7c2a7ea14b5ea0b9c2fd98da64895a7efe91
SameNoName <61697143+SameNoName@users.noreply.github.com>
2021-10-21 17:43:05 +0500

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 #каталог с файлами HTML и подкаталогом cgi-bin для сценариев
-webdir = r'..\5'
+webdir = r'5'
 port = 80
 os.chdir(webdir) #перейти в корневой веб-каталог
 srvraddr = ('', port) # имя хоста (или IP), номер порта
